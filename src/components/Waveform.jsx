9c20dd4b442ff3cb6f5efe2875999441061f7098
AnshVM <anshmalik2002@gmail.com>
2021-12-04 17:47:21 +0530

    Pause playhead when audio is paused

--- src/components/Waveform.jsx
+++ src/components/Waveform.jsx
@@ -6,6 +6,10 @@ export default function Waveform(props) {
     const audioCtx = props.audioCtx
     const [imgurl, setImgurl] = useState()
     let duration = useRef()
+    let startTime = useRef()
+    let pauseTime = useRef()
+    let playhead = useRef()
+
 
     const drawLine = (ctx, currentx, height) => {
         ctx.beginPath();
@@ -50,12 +54,13 @@ export default function Waveform(props) {
         ctx.clearRect(0, 0, 1920, 150)
     }
 
-
     const movePlayHead = () => {
         const playHead = document.getElementById('playHead')
         const transitionDuration = Math.floor(duration.current*1000)
-        playHead.style.transition = `transform ${transitionDuration}ms linear`
-        playHead.style.transform = 'translate(1920px)'
+        playHead.style.transition = `margin-left ${transitionDuration}ms linear`
+        const marginLeft = 1920 - Number(playhead.current.style.marginLeft[0,playhead.current.style.marginLeft.length-3])
+        
+        playHead.style.marginLeft = `${marginLeft}px`
     }
 
     useEffect(() => {
@@ -81,17 +86,28 @@ export default function Waveform(props) {
 
     useEffect(() => {
 
+        if(props.isPlaying === false) {
+            const marginLeft = window.getComputedStyle(playhead.current).getPropertyValue('margin-left')
+            playhead.current.style.transform = ""
+            playhead.current.style.transition = ""     
+            playhead.current.style.marginLeft = marginLeft
+            pauseTime.current = Date.now()
+            duration.current = duration.current - (pauseTime.current/1000 - startTime.current/1000)
+        }
+
         if(props.isPlaying === true) {
+            startTime.current = Date.now()
             movePlayHead()
         }
 
     }, [props.isPlaying])
 
+
     return (
         <div>
             <h1>Waveform</h1>
             <div style={{backgroundColor:"black",width:'1920px',height:'300px'}} id="waveform">
-                <div style={{width:'1px',height:'300px',backgroundColor:'red'}} id="playHead"></div>
+                <div ref={playhead} style={{width:'1px',height:'300px',backgroundColor:'red'}} id="playHead"></div>
             </div>
             <canvas id='canvas' width="1920px" height="300px"></canvas>
 
